b1780d7e36c78df89c8f44064cf5f97f221d0de2
Ranxi Verba <ranxi.verba@gmail.com>
2019-12-10 16:21:59 +0200

    add trivial implementations

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,30 @@ where
     fn clone_array(a: &GenericArray<u8, Self::Length>) -> Self;
 }
 
+impl<L> LineValid for GenericArray<u8, L>
+where
+    L: ArrayLength<u8>,
+{
+    type Length = L;
+
+    fn try_clone_array(a: &GenericArray<u8, Self::Length>) -> Result<Self, ()> {
+        Ok(a.clone())
+    }
+
+    fn clone_line(&self) -> GenericArray<u8, Self::Length> {
+        self.clone()
+    }
+}
+
+impl<L> Line for GenericArray<u8, L>
+where
+    L: ArrayLength<u8>,
+{
+    fn clone_array(a: &GenericArray<u8, Self::Length>) -> Self {
+        a.clone()
+    }
+}
+
 pub struct Concat<U, V>(pub U, pub V)
 where
     U: LineValid,
