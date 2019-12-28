0422a340376d129828e41c114d709e211e890698
Ranxi Verba <ranxi.verba@gmail.com>
2019-12-28 19:42:31 +0200

    add 25519

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,9 @@ pub use self::symmetric::{Tag, Key};
 #[cfg(feature = "secp256k1")]
 mod secp256k1_m;
 
+#[cfg(feature = "curve25519-dalek")]
+mod curve25519_dalek_m;
+
 #[cfg(all(feature = "chacha20-poly1305-aead", feature = "use_std"))]
 mod chacha20_poly1305_aead_m;
 #[cfg(all(feature = "chacha20-poly1305-aead", feature = "use_std"))]
